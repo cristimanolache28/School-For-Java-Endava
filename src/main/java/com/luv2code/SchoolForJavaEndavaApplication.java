613ce27f2d32b42ff3bd3016fae97aad8e3ffa88
Cristian Manolache <cristianmanolache0@gmail.com>
2022-03-10 13:20:42 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/SchoolForJavaEndavaApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolForJavaEndavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolForJavaEndavaApplication.class, args);
+    }
+
+}
